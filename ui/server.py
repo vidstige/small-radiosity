6c414d31ad659c0d350bcc67e5edd18a4fc46820
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-07 22:18:34 +0100

    slider to update photons

--- ui/server.py
+++ ui/server.py
@@ -1,11 +1,12 @@
 import subprocess
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='')
 
 @app.route('/render/cornel-box/250x250')
 def render():
-    subprocess.check_call('../build/small_radiosity')
+    photons = request.args.get('photons', "10000")
+    subprocess.check_call(['../build/small_radiosity', '--photons', photons])
     subprocess.check_call('convert cameraImage.ppm cameraImage.png'.split())
     return app.send_static_file('cameraImage.png')
 
