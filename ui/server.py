f8b0ac215afb012099966f83d37b99bdba862cc5
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-10 16:51:38 +0100

    compile program

--- ui/server.py
+++ ui/server.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='')
 
-@app.route('/render/cornel-box/250x250')
-def render():
+@app.route('/render/cornel-box/<int:width>x<int:height>')
+def render(width, height):
+    # 1. compile
+    subprocess.check_call(['g++', '-std=c++11', '../main.cpp', '-o', 'small_radiosity'])
+
+    # 2. run
     photons = request.args.get('photons', "10000")
     subprocess.check_call(['../build/small_radiosity', '--photons', photons])
     subprocess.check_call('convert cameraImage.ppm cameraImage.png'.split())
