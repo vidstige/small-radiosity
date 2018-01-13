de98ede28d39b19329063df9b3386bd57c750576
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-13 18:45:12 +0100

    knockout.js

--- ui/static/ui.js
+++ ui/static/ui.js
@@ -1,33 +1,45 @@
+var ViewModel = function() {
+    this.photonsPosition = ko.observable(50);
+    this.size = ko.observable(256);
+ 
+    this.imageUrl = ko.observable(null);
 
-// https://stackoverflow.com/questions/846221/logarithmic-slider
-function exponentialSlider(id, a, b) {
-    // position will be between 0 and 100
-    const el = document.getElementById(id);
-    var minp = el.min;
-    var maxp = el.max;
+    this.buildUrl = function(action) {
+        var size = this.size();
+        return "/" + action + "/cornel-box/" + size + "x" + size + "?photons=" + this.photons();
+    };
+    this.render = function() {
+        this.imageUrl(this.buildUrl('render'));
+    };
+    this.estimate = function() {
+        fetch(this.buildUrl('estimate')).then(function (r) { return r.text(); }).then(console.log);
+    };
+    this.photons = ko.pureComputed(function() {
+        // https://stackoverflow.com/questions/846221/logarithmic-slider
+        // position will be between 0 and 100
+        var minp = 0;
+        var maxp = 100;
 
-    // The result should be between a and b
-    var minv = Math.log(a);
-    var maxv = Math.log(b);
+        // The result should be between 100 and 1000000
+        var minv = Math.log(100);
+        var maxv = Math.log(1000000);
 
-    // calculate adjustment factor
-    var scale = (maxv - minv) / (maxp - minp);
+        // calculate adjustment factor
+        var scale = (maxv - minv) / (maxp - minp);
 
-    return Math.exp(minv + scale * (el.value - minp));
+        var value =  Math.exp(minv + scale * (this.photonsPosition() - minp));
+        return Math.round(value);        
+    }, this);
+};
+
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
 }
 
-function buildUrl(action) {
-    var photons = Math.round(exponentialSlider('photons', 1, 1000000));
-    var size = document.getElementById('size').value;
-    return "/"+action+"/cornel-box/" + size + "x" + size + "?photons=" + photons;
-}
-
-function update() {
-    var target = document.getElementById('target');
-    target.src = buildUrl('render');
-    console.log(target.src);
-}
-
-function estimate() {
-    fetch(buildUrl('estimate')).then(function (r) { return r.text(); }).then(console.log);
-}
+ready(function() {
+    ko.applyBindings(new ViewModel());    
+});
