b1b1c7ac8214977a538c16024fcf9a8e1a8410d0
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-01-15 13:16:07 +0100

    ex: improved run.sh

--- ui/run.sh
+++ ui/run.sh
@@ -1,1 +1,8 @@
-FLASK_APP=server.py FLASK_DEBUG=1 venv/bin/flask run --reload
+#!/bin/sh
+set -e
+if [ ! -d venv/ ]; then
+    python3 -m venv venv
+    venv/bin/python3 -m pip install -r requirements.txt
+fi
+. venv/bin/activate
+FLASK_APP=server.py FLASK_DEBUG=1 flask run --reload
